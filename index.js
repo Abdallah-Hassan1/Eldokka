34413db6527ae2f34cf22e78eadaf714fa72f337
Abdallah Hassan <91224794+Abdallah-Hassan1@users.noreply.github.com>
2026-04-06 19:31:53 +0200

    language

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+function setClass(element, value){
+    element.setAttribute("class", value);
+}
+
 function setLanguage(lang) {
   if (lang === 'ar') {
     document.body.dir = "rtl";
     document.getElementById("title").innerText = "مطعم الدوكه";
+    setClass(document.getElementsByClassName("language")[0], "language");
+    setClass(document.getElementsByClassName("language")[1], "language active");
     document.getElementById("about-title").innerText = "من نحن";
     document.getElementById("about-text").innerText = "مرحبًا بكم في مطعم الدوكة. نقدم أشهى المأكولات الشرقية والغربية في أجواء مميزة.";
     document.getElementById("contact-title").innerText = "اتصل بنا";
@@ -9,6 +15,8 @@ function setLanguage(lang) {
   } else {
     document.body.dir = "ltr";
     document.getElementById("title").innerText = "Eldokka Restaurant";
+    setClass(document.getElementsByClassName("language")[0], "language active");
+    setClass(document.getElementsByClassName("language")[1], "language");
     document.getElementById("about-title").innerText = "About Us";
     document.getElementById("about-text").innerText = "Welcome to Eldokka Restaurant. We serve delicious Eastern and Western dishes with a cozy atmosphere.";
     document.getElementById("contact-title").innerText = "Contacts";
